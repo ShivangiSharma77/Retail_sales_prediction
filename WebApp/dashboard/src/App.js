8136d3cae5e0d80e40f942fce8c44c4168bb7c6e
Shivangi Sharma <62565739+ShivangiSharma77@users.noreply.github.com>
2020-05-13 15:05:57 +0530

    Update App.js

--- WebApp/dashboard/src/App.js
+++ WebApp/dashboard/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import SalesPrediction from "./SalesPrediction";
 function App() {
   return (
     <div className="App">
-      React Boiler Plate
+            <SalesPrediction />
+            </div>
     </div>
   );
 }
